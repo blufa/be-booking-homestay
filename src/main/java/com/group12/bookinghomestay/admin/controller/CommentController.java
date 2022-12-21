47cbc0338cca2998df23a8a21e2c5da55d5c765c
Hieu Nguyen <nhocproaoe@gmail.com>
2022-12-21 15:16:42 +0700

    add comment + review function

--- src/main/java/com/group12/bookinghomestay/admin/controller/CommentController.java
+++ src/main/java/com/group12/bookinghomestay/admin/controller/CommentController.java
@@ -3,10 +3,7 @@ package com.group12.bookinghomestay.admin.controller;
 import com.group12.bookinghomestay.admin.model.Comment;
 import com.group12.bookinghomestay.admin.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,15 +11,23 @@ import java.util.List;
 public class CommentController {
     @Autowired
     private CommentService commentService;
-    private static final String PATH="/comments";
+    private static final String PATH = "/comments";
+
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(PATH)
     public List<Comment> getCommentList() {
         return commentService.findAll();
     }
+
     @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping(PATH+"/{id}/hotel")
+    @GetMapping(PATH + "/{id}/hotel")
     public List<Comment> getCommentListByHoteId(@PathVariable(name = "id") Integer id) {
         return commentService.findByHotelId(id);
+    }
+
+    @CrossOrigin("*")
+    @PostMapping(PATH + "/addComment")
+    public Comment addComment(@RequestBody Comment comment) {
+        return commentService.save(comment);
     }
 }
--- src/main/java/com/group12/bookinghomestay/admin/repository/ReviewRepository.java
+++ src/main/java/com/group12/bookinghomestay/admin/repository/ReviewRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query(value = "select * from review group by hotel_id having avg(rate) > 3", nativeQuery = true)
+    @Query(value = "from Review group by hotel.id having avg(rate) > 3")
     List<Review> getHotelHasGoodRating();
+
+    @Query("from Review where hotel.id = ?1")
+    List<Review> getReviewByHotelId(Long id);
 }
